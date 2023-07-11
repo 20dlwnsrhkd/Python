ae4bf9e224f0c4f991f1d1997d22e2b501cf14db
이준광 <jimmy010011@gmail.com>
2023-07-11 04:31:37 +0000

    자료형: 리스트 컴프리헨션

---
+++ Day1/DATA_TYPE_EX7.py
@@ -0,0 +1,7 @@
+array=[]
+while(True):
+    a = input("문자열을 입력해주세요: ")
+    array.append(a)
+    if(a==''):
+        print(array)
+        break
--- Day1/IF_REPEAT_EX5.py
+++ Day1/IF_REPEAT_EX5.py
@@ -1,4 +1,5 @@
 i = int(input("입력받은 x부터 9단까지 구구단 출력 : "))
 for i in range(i,10):
-    for 9 in j:
+    print("\n")
+    for j in range(1,10):
         print(i*j)
